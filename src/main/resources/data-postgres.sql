602cb7fc629b68dbf93b0000ee1094579fb412da
paulmcg421 <paulmcg421@gmail.com>
2020-08-30 16:03:34 +0100

    Made new databse user

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -1,4 +1,4 @@
 INSERT INTO "User" (name,age,user_id,last_updated,email) values
 ('Paul McGranaghan',29,'U1','2020-07-16 15:04:41.512378','paulmcg421@gmail.com'),
-('Darren McGranaghan',29,'U3',CURRENT_TIMESTAMP,'darrenmcg1@gmail.com');
+('Darren McGranaghan',29,'U2',CURRENT_TIMESTAMP,'darrenmcg1@gmail.com');
 
