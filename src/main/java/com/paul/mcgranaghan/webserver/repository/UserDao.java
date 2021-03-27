84d8fcd4ada6d26f9c39031e43695724d3bac836
paulmcg421 <paulmcg421@gmail.com>
2021-03-27 18:53:54 +0000

    Logging working

--- src/main/java/com/paul/mcgranaghan/webserver/repository/UserDao.java
+++ src/main/java/com/paul/mcgranaghan/webserver/repository/UserDao.java
@@ -2,6 +2,8 @@ package com.paul.mcgranaghan.webserver.repository;
 
 import com.paul.mcgranaghan.webserver.dto.User;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -12,26 +14,28 @@ import java.util.List;
 import java.util.Map;
 
 @Repository
-//@Slf4j
 @RequiredArgsConstructor
 public class UserDao {
+
+    private final static Log log = LogFactory.getLog(UserDao.class);
 
     private static final String GET_ALL_USERS_SQL = "SELECT * FROM \"User\"";
     private static final String INSERT_INTO_USER_SQL = "INSERT INTO \"User\" (user_id, age, name ,email, last_updated) values (:user_id, :age, :name, :email, CURRENT_TIMESTAMP)";
     private static final String GET_NEXT_USER_SEQ_SQL = "SELECT nextval('public.User_ID_Seq')";
     private static final String GET_USER_BY_ID = GET_ALL_USERS_SQL + " WHERE user_id = :user_id";
-
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     private final RowMapper<User> userRowMapper = new UserRowMapper();
 
+
     public void save(User entity) {
+        String userId = getNextUserId();
+
         Map<String, Object> paramMap = new HashMap<>();
         paramMap.put("name", entity.getName());
         paramMap.put("email", entity.getEmail());
-        paramMap.put("user_id", getNextUserId());
+        paramMap.put("user_id", userId);
         paramMap.put("age", entity.getAge());
-
-        //log.info("Inserting new User {}" + entity.id);
+        log.info("Inserting new User: " + userId);
         namedParameterJdbcTemplate.update(INSERT_INTO_USER_SQL, paramMap);
     }
 
@@ -43,22 +47,23 @@ public class UserDao {
             User requestedUser = namedParameterJdbcTemplate.queryForObject(GET_USER_BY_ID, paramMap, User.class);
             return requestedUser != null;
         } catch (DataAccessException e) {
-            //log.error("Cannot get user id={}", id, e);
+            log.error("Cannot get user id: " + id, e);
         }
         return false;
     }
 
     private String getNextUserId() {
+        log.info("Getting next user id");
         Map<String, Object> paramMap = new HashMap<>();
         return "U" + namedParameterJdbcTemplate.queryForObject(GET_NEXT_USER_SEQ_SQL, paramMap, Integer.class);
     }
 
     public List<User> findAll() {
-        //log.info("Requesting all user info");
+        log.info("Requesting all user info");
         try {
             return namedParameterJdbcTemplate.query(GET_ALL_USERS_SQL, userRowMapper);
         } catch (DataAccessException e) {
-            //log.error("Cannot access user info", e);
+            log.error("Cannot access user info", e);
         }
         return null;
     }
