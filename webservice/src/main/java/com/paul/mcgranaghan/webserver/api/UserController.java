e8b6fff738934aae8caaeca6c72624c8f3159dd2
paulmcg421 <paulmcg421@gmail.com>
2021-06-28 19:44:23 +0100

    Initial working rabbitMq

--- webservice/src/main/java/com/paul/mcgranaghan/webserver/api/UserController.java
+++ webservice/src/main/java/com/paul/mcgranaghan/webserver/api/UserController.java
@@ -2,9 +2,6 @@ package com.paul.mcgranaghan.webserver.api;
 
 import com.paul.mcgranaghan.webserver.dto.User;
 import com.paul.mcgranaghan.webserver.repository.UserDao;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -20,7 +16,6 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
-    private final static String QUEUE_NAME = "rabbit@f26b8953c06b";
     private final UserDao userDao;
 
     @GetMapping("/users/listAll")
@@ -31,17 +26,6 @@ public class UserController {
     @PostMapping("/users/addUser")
     void addUser(@RequestBody User user) {
         userDao.save(user);
-    }
-
-    void receiveMessage() throws IOException {
-        ConnectionFactory factory = new ConnectionFactory();
-        //factory.setHost("localhost");
-        Connection connection = factory.newConnection("http://192.168.1.86:15672/#/");
-        Channel channel = connection.createChannel();
-        channel.queueDeclare(1, QUEUE_NAME);
-
-        //channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
     }
 
     public boolean existsById(String id) {
---
+++ webservice/src/main/java/com/paul/mcgranaghan/webserver/config/MqConfig.java
@@ -0,0 +1,58 @@
+package com.paul.mcgranaghan.webserver.config;
+
+import com.paul.mcgranaghan.webserver.mq.Receiver;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MqConfig {
+    static final String topicExchangeName = "spring-boot-exchange";
+
+    static final String queueName = "rabbit@f26b8953c06b";
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(topicExchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                             MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+
+    @Bean
+    ConnectionFactory connectionFactory() {
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory();
+        cachingConnectionFactory.setAddresses("192.168.1.86:5672");
+        return cachingConnectionFactory;
+    }
+
+}
