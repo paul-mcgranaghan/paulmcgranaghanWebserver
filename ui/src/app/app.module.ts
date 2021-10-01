04fae8bf25b3eb7838f179b09f56d1e2575a8eab
paulmcg421 <paulmcg421@gmail.com>
2021-10-01 22:29:20 +0100

    Heaps of stuff: - Grade Tweaks - springdocs - Dark mode option

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {UserListComponent} from './user-list/user-list.component';
 import {UserFormComponent} from './user-form/user-form.component';
-import {UserService} from './service/user.service';
+import {UserService} from './service/user/user.service';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {RouterModule} from '@angular/router';
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatPseudoCheckboxModule} from "@angular/material/core";
 
 @NgModule({
   declarations: [
@@ -26,7 +28,10 @@ import {RouterModule} from '@angular/router';
     BrowserAnimationsModule,
     MatFormFieldModule,
     MatIconModule,
-    RouterModule.forRoot([])
+    RouterModule.forRoot([]),
+    MatSlideToggleModule,
+    ReactiveFormsModule,
+    MatPseudoCheckboxModule
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
--- webservice/src/main/java/com/paul/mcgranaghan/webserver/api/UserController.java
+++ webservice/src/main/java/com/paul/mcgranaghan/webserver/api/UserController.java
@@ -2,28 +2,43 @@ package com.paul.mcgranaghan.webserver.api;
 
 import com.paul.mcgranaghan.webserver.dto.User;
 import com.paul.mcgranaghan.webserver.repository.UserDao;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/users")
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     private final UserDao userDao;
 
-    @GetMapping("/users/listAll")
+    @Operation(summary = "List all the users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found users",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = User.class))}),
+            @ApiResponse(responseCode = "404", description = "Users not found",
+                    content = @Content)})
+    @GetMapping("/listAll")
     public List<User> getUsers() {
         return userDao.findAll();
     }
 
-    @PostMapping("/users/addUser")
+    @Operation(summary = "Add a user to the database")
+    @PostMapping("/addUser")
     void addUser(@RequestBody User user) {
         userDao.save(user);
     }
