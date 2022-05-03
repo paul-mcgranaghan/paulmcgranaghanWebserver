5dbe507d05c658b1ed4292f4c6d09fe73d322497
Paul McGranaghan <paulmcg421@gmail.com>
2022-05-03 18:57:15 +0100

    Fixing import

--- ui/src/app/service/user/user.service.ts
+++ ui/src/app/service/user/user.service.ts
@@ -1,13 +1,13 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {User} from '../../model/user';
-import {Observable} from 'rxjs/Observable';
+import {Observable} from 'rxjs';
 
 @Injectable()
 export class UserService {
 
-  private usersUrl: string;
-  private addUserUrl: string;
+  private readonly usersUrl: string;
+  private readonly addUserUrl: string;
   private login: string;
 
   constructor(private http: HttpClient) {
