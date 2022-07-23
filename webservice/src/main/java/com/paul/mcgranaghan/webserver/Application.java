c8dbf3919a4d1a99414941959429f503bf4b7980
Paul McGranaghan <paulmcg421@gmail.com>
2022-07-23 15:47:29 +0100

    Removing old imports

--- webservice/src/main/java/com/paul/mcgranaghan/webserver/Application.java
+++ webservice/src/main/java/com/paul/mcgranaghan/webserver/Application.java
@@ -3,13 +3,10 @@ package com.paul.mcgranaghan.webserver;
 import com.paul.mcgranaghan.webserver.config.MongoConfig;
 import com.paul.mcgranaghan.webserver.config.MqConfig;
 import com.paul.mcgranaghan.webserver.config.SecurityConfig;
-import com.paul.mcgranaghan.webserver.repository.NameBasicsDao;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 /*@EnableMongoRepositories*/
 @SpringBootApplication
